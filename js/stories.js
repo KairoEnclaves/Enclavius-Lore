8b36b2f629fdba6d9c47accf25044995aab07035
KairoEnclaves <59368266+KairoEnclaves@users.noreply.github.com>
2025-10-20 01:07:55 +0200

    Update stories.js

--- js/stories.js
+++ js/stories.js
@@ -7,6 +7,13 @@ window.STORIES = [
     cover: "https://i.gyazo.com/fef408c21a3b134bd8fff7dc56adcdc6.jpg", // optional
     badge: "" // optional (example: "New")
   },
+  {
+    slug: "invictus",
+    title: "The Tale of Invictus",
+    subtitle: "How Gens Enclavius came to be reborn.",
+    cover: "https://i.gyazo.com/036674aba044ffaad9ac1b279d3ad564.png", // optional
+    badge: "" // optional (example: "New")
+  },
   // Add more stories:
   // { slug: "another-story", title: "Another Story", subtitle: "Optional", cover: "https://...", badge: "" },
 ];
